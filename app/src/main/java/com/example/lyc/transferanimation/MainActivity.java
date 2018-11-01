ea13927b8fb49ac1244d73dc1f906e021546598e
Luoyc <463797482@qq.com>
2018-11-01 10:46:12 +0800

    git

---
+++ app/src/main/java/com/example/lyc/transferanimation/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lyc.transferanimation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
